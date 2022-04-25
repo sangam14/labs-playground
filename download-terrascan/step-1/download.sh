3da070d68efc41c88d238815dedbe354aaf0ab0f
Sangam Biradar <sangam14@users.noreply.github.com>
2022-04-25 22:16:17 +0530

    Create download.sh

---
+++ download-terrascan/step-1/download.sh
@@ -0,0 +1,6 @@
+# bin/bash 
+
+curl -L "$(curl -s https://api.github.com/repos/accurics/terrascan/releases/latest | grep -o -E "https://.+?_Darwin_x86_64.tar.gz")" > terrascan.tar.gz
+tar -xf terrascan.tar.gz terrascan && rm terrascan.tar.gz
+install terrascan /usr/local/bin && rm terrascan
+terrascan
