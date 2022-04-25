919f7fa403b9f44d5073db37fd7b8eee8f664641
sangam14 <sangam14@users.noreply.github.com>
2022-04-26 00:51:45 +0530

    terrascan-k8s

--- install-terrascan/foreground.sh
+++ install-terrascan/foreground.sh
@@ -1,4 +1,7 @@
 curl -L "$(curl -s https://api.github.com/repos/accurics/terrascan/releases/latest | grep -o -E "https://.+?_Darwin_x86_64.tar.gz")" > terrascan.tar.gz
-tar -xf terrascan.tar.gz terrascan && rm terrascan.tar.gz
-install terrascan /usr/local/bin && rm terrascan
+tar -xf terrascan.tar.gz 
+terrascan 
+rm terrascan.tar.gz
+install terrascan /usr/local/bin 
+rm terrascan
 terrascan
